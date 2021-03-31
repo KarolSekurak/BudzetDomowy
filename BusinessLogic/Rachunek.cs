d34ebc699b74ab152197248391d4060b0e8cbd51
Karol 108577 <pzx94389@student.wsb.poznan.pl>
2021-03-31 18:49:23 +0200

    po pracy

---
+++ BusinessLogic/Rachunek.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace BudzetDomowy
+{
+    [DataContract]
+    class Rachunek : XmlStorage<Rachunek>
+    {
+        [DataMember]
+        public int id_rachunku { get; set; }
+        [DataMember]
+        public DateTime data_wprowadzenia { get; set; }
+        [DataMember]
+        public int kwota { get; set; }
+        [DataMember]
+        static public int KtoWprowadzil { get; set; }
+        [DataMember]
+        static public int KogoDotyczy { get; set; }
+        [DataMember]
+        public string kategoria { get; set; }
+
+
+
+        static public void DodajRachunek(int _id_rachunku, DateTime _data_wprowadzenia, int _Kwota, int _KtoWprowadzil, int _KogoDotyczy) { }
+        
+        static public int WyszukajRachunek() { return 0; }
+        static public void UsunRachunek(int _id_rachunku) { }
+        static public void EdytujRachunek(int _id_rachunku) { }
+        static public void WyswietlRachunek(int _id_rachunku) { }
+
+        public Rachunek()
+        {
+            this.BaseObject = this;
+        }
+
+        public override bool InitializeFromObject(Rachunek Object)
+        {
+            //this.Login = Object.Login;
+            //this.Password = Object.Password;
+
+            return true;
+        }
+
+    }
+}
--- BusinessLogic/User.cs
+++ BusinessLogic/User.cs
@@ -16,6 +16,21 @@ namespace BudzetDomowy.BusinessLogic
         public string Login { get; set; }
         [DataMember]
         public string Password { get; set; }
+        [DataMember]
+        public string Imie { get; set; }
+        [DataMember]
+        public string Nazwisko { get; set; }
+        [DataMember]
+        public int id_usera { get; set; }
+        [DataMember]
+        public DateTime DodanieUzytkownika { get; set; }
+
+
+        static public void DodajUsera(string _login, string _haslo, string _Imie, string _Nazwisko, int _id_usera, DateTime _DodanieUzytkownika) { }
+        static public int WyszukajUsera() { return 0; }
+        static public void UsunUsera(int _id_usera) { }
+        static public void EdytujUsera(int _id_usera) { }
+        static public void WyswietlUsera(int _id_usera) { }
 
         public User()
         {
