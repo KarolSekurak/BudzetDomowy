ad721f37e5d6cc34373c6735acb1886073648794
Karol 108577 <pzx94389@student.wsb.poznan.pl>
2021-03-31 18:11:10 +0200

    Test1

---
+++ BusinessLogic/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace BudzetDomowy.BusinessLogic
+{
+    [DataContract]
+    public class User : XmlStorage<User>
+    {
+        [DataMember]
+        public string Login { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+
+        public User()
+        {
+            this.BaseObject = this;
+        }
+
+        public override bool InitializeFromObject(User Object)
+        {
+            this.Login = Object.Login;
+            this.Password = Object.Password;
+
+            return true;
+        }
+    }
+}
---
+++ IXmlStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetDomowy
+{
+    public interface IXmlStorage
+    {
+        bool FromXml(Stream Stream);
+        MemoryStream ToXml();
+
+    }
+}
